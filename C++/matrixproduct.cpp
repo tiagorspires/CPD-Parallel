edbc25b9441c84f90f1fc51811ecfb2e0d74a869
Tiago Pires <blackalbino17@gmail.com>
2024-02-27 15:30:47 +0000

    part 2 done

--- C++/matrixproduct.cpp
+++ C++/matrixproduct.cpp
@@ -14,7 +14,7 @@ void OnMult(int m_ar, int m_br, int EventSet)
 {
 	
 	SYSTEMTIME Time1, Time2;
-	long long values[3];
+	long long values[2];
 	
 	char st[100];
 	double temp;
@@ -63,7 +63,7 @@ void OnMult(int m_ar, int m_br, int EventSet)
   	if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
   	printf("L1 DCM: %lld \n",values[0]);
   	printf("L2 DCM: %lld \n",values[1]);
-	printf("DP OPS: %lld \n",values[2]);
+
 
     Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
@@ -86,7 +86,7 @@ void OnMult(int m_ar, int m_br, int EventSet)
 void OnMultLine(int m_ar, int m_br, int EventSet)
 {
     SYSTEMTIME Time1, Time2;
-	long long values[3];
+	long long values[2];
 	
 	char st[100];
 	double temp;
@@ -139,7 +139,6 @@ void OnMultLine(int m_ar, int m_br, int EventSet)
   	if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
   	printf("L1 DCM: %lld \n",values[0]);
   	printf("L2 DCM: %lld \n",values[1]);
-	printf("DP OPS: %lld \n",values[2]);
 
     Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
@@ -162,7 +161,7 @@ void OnMultLine(int m_ar, int m_br, int EventSet)
 void OnMultBlock(int m_ar, int m_br, int bkSize, int EventSet)
 {
     SYSTEMTIME Time1, Time2;
-	long long values[3];
+	long long values[2];
 	
 	char st[100];
 	double temp;
@@ -221,7 +220,6 @@ void OnMultBlock(int m_ar, int m_br, int bkSize, int EventSet)
   	if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
   	printf("L1 DCM: %lld \n",values[0]);
   	printf("L2 DCM: %lld \n",values[1]);
-	printf("DP OPS: %lld \n",values[2]);
     
 	Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
@@ -239,6 +237,160 @@ void OnMultBlock(int m_ar, int m_br, int bkSize, int EventSet)
     free(phb);
     free(phc);
 }
+
+void OnMultLine_Parallel1(int m_ar, int m_br, int EventSet){
+	
+	SYSTEMTIME Time1, Time2;
+	long long values[2];
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+	
+
+	
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phc[i*m_br + j] = 0;
+
+
+
+    Time1 = clock();
+
+	// Start counting
+	int ret;
+	ret = PAPI_start(EventSet);
+	if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
+
+	#pragma omp parallel for private(i, j, k)
+	for(i=0; i<m_ar; i++)
+	{	for( k=0; k<m_ar; k++)
+		{
+			for( j=0; j<m_br; j++)
+			{	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+	ret = PAPI_stop(EventSet, values);
+  	if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
+  	printf("L1 DCM: %lld \n",values[0]);
+  	printf("L2 DCM: %lld \n",values[1]);
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+
+}
+
+void OnMultLine_Parallel2(int m_ar, int m_br, int EventSet){
+	
+	SYSTEMTIME Time1, Time2;
+	long long values[2];
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+	
+
+	
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phc[i*m_br + j] = 0;
+
+
+
+    Time1 = clock();
+
+	// Start counting
+	int ret;
+	ret = PAPI_start(EventSet);
+	if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
+
+	#pragma omp parallel for private(i, j, k)
+	for(i=0; i<m_ar; i++)
+	{	for( k=0; k<m_ar; k++)
+		{
+			#pragma omp for  
+			for( j=0; j<m_br; j++)
+			{	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+	ret = PAPI_stop(EventSet, values);
+  	if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
+  	printf("L1 DCM: %lld \n",values[0]);
+  	printf("L2 DCM: %lld \n",values[1]);
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+
+}
+
 
 
 
@@ -288,14 +440,13 @@ int main (int argc, char *argv[])
 	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
 	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
 
-	ret = PAPI_add_event(EventSet,PAPI_DP_OPS);
-	if (ret != PAPI_OK) cout << "ERROR: PAPI_DP_OPS" << endl;
-
 	op=1;
 	do {
 		cout << endl << "1. Multiplication" << endl;
 		cout << "2. Line Multiplication" << endl;
 		cout << "3. Block Multiplication" << endl;
+		cout << "4. Line Multiplication Parallel 1" << endl;
+		cout << "5. Line Multiplication Parallel 2" << endl;
 		cout << "Selection?: ";
 		cin >>op;
 		if (op == 0)
@@ -316,7 +467,12 @@ int main (int argc, char *argv[])
 				cin >> blockSize;
 				OnMultBlock(lin, col, blockSize, EventSet);  
 				break;
-
+			case 4:
+				OnMultLine_Parallel1(lin, col, EventSet);
+				break;
+			case 5:	
+				OnMultLine_Parallel2(lin, col, EventSet);
+				break;
 		}
 
 		ret = PAPI_reset( EventSet );
@@ -332,10 +488,6 @@ int main (int argc, char *argv[])
 		std::cout << "FAIL remove event" << endl; 
 
 	ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
-	if ( ret != PAPI_OK )
-		std::cout << "FAIL remove event" << endl; 
-	
-	ret = PAPI_remove_event( EventSet, PAPI_DP_OPS);
 	if ( ret != PAPI_OK )
 		std::cout << "FAIL remove event" << endl; 
 
